3cc5c9d6aacf48269ad96051cd3f479c775aaf4f
Joshua-Luke Bevan <joshua-luke@hotmail.co.uk>
2020-07-28 20:20:07 +0100

    Added initial pytest

--- number_to_lcd_test.py
+++ number_to_lcd_test.py
@@ -5,6 +5,7 @@ from number_to_lcd import print_number_from_representation
 
 import sys
 import unittest
+import pytest
 
 
 @contextmanager
@@ -18,73 +19,81 @@ def captured_output():
         sys.stdout, sys.stderr = old_out, old_err
 
 
-class NumberToLCDTestCase(unittest.TestCase):
+def test_displays_the_number_one_on_the_lcd():
+    with captured_output() as (out, err):
+        print_number_from_representation(number_to_lcd_representation(1))
+        output = out.getvalue()
+        assert output == " \n|\n|\n"
 
-    def test_displays_the_number_one_on_the_lcd(self):
-        with captured_output() as (out, err):
-            print_number_from_representation(number_to_lcd_representation(1))
-            output = out.getvalue()
-            self.assertEqual(output, " \n|\n|\n")
 
-    def test_displays_the_number_two_on_the_lcd(self):
-        with captured_output() as (out, err):
-            print_number_from_representation(number_to_lcd_representation(2))
-            output = out.getvalue()
-            self.assertEqual(output, "_ \n_|\n|_\n")
+def test_displays_the_number_two_on_the_lcd():
+    with captured_output() as (out, err):
+        print_number_from_representation(number_to_lcd_representation(2))
+        output = out.getvalue()
+        assert output == "_ \n_|\n|_\n"
 
-    def test_displays_the_number_three_on_the_lcd(self):
-        with captured_output() as (out, err):
-            print_number_from_representation(number_to_lcd_representation(3))
-            output = out.getvalue()
-            self.assertEqual(output, "_ \n_|\n_|\n")
 
-    def test_displays_the_number_four_on_the_lcd(self):
-        with captured_output() as (out, err):
-            print_number_from_representation(number_to_lcd_representation(4))
-            output = out.getvalue()
-            self.assertEqual(output, "   \n|_|\n  |\n")
+def test_displays_the_number_three_on_the_lcd():
+    with captured_output() as (out, err):
+        print_number_from_representation(number_to_lcd_representation(3))
+        output = out.getvalue()
+        assert output == "_ \n_|\n_|\n"
 
-    def test_displays_the_number_five_on_the_lcd(self):
-        with captured_output() as (out, err):
-            print_number_from_representation(number_to_lcd_representation(5))
-            output = out.getvalue()
-            self.assertEqual(output, " _ \n|_ \n _|\n")
 
-    def test_displays_the_number_six_on_the_lcd(self):
-        with captured_output() as (out, err):
-            print_number_from_representation(number_to_lcd_representation(6))
-            output = out.getvalue()
-            self.assertEqual(output, "   \n|_ \n|_|\n")
+def test_displays_the_number_four_on_the_lcd():
+    with captured_output() as (out, err):
+        print_number_from_representation(number_to_lcd_representation(4))
+        output = out.getvalue()
+        assert output == "   \n|_|\n  |\n"
 
-    def test_displays_the_number_seven_on_the_lcd(self):
-        with captured_output() as (out, err):
-            print_number_from_representation(number_to_lcd_representation(7))
-            output = out.getvalue()
-            self.assertEqual(output, "_ \n |\n |\n")
 
-    def test_displays_the_number_eight_on_the_lcd(self):
-        with captured_output() as (out, err):
-            print_number_from_representation(number_to_lcd_representation(8))
-            output = out.getvalue()
-            self.assertEqual(output, " _ \n|_|\n|_|\n")
+def test_displays_the_number_five_on_the_lcd():
+    with captured_output() as (out, err):
+        print_number_from_representation(number_to_lcd_representation(5))
+        output = out.getvalue()
+        assert output == " _ \n|_ \n _|\n"
 
-    def test_displays_the_number_nine_on_the_lcd(self):
-        with captured_output() as (out, err):
-            print_number_from_representation(number_to_lcd_representation(9))
-            output = out.getvalue()
-            self.assertEqual(output, " _\n|_|\n  |\n")
 
-    def test_displays_the_number_zero_on_the_lcd(self):
-        with captured_output() as (out, err):
-            print_number_from_representation(number_to_lcd_representation(0))
-            output = out.getvalue()
-            self.assertEqual(output, " _ \n| |\n|_|\n")
+def test_displays_the_number_six_on_the_lcd():
+    with captured_output() as (out, err):
+        print_number_from_representation(number_to_lcd_representation(6))
+        output = out.getvalue()
+        assert output == "   \n|_ \n|_|\n"
 
-    def test_displays_the_number_ten_on_the_lcd(self):
-        with captured_output() as (out, err):
-            print_number_from_representation(number_to_lcd_representation(10))
-            output = out.getvalue()
-            self.assertEqual(output, "   _ \n| | |\n| |_|\n")
+
+def test_displays_the_number_seven_on_the_lcd():
+    with captured_output() as (out, err):
+        print_number_from_representation(number_to_lcd_representation(7))
+        output = out.getvalue()
+        assert output == "_ \n |\n |\n"
+
+
+def test_displays_the_number_eight_on_the_lcd():
+    with captured_output() as (out, err):
+        print_number_from_representation(number_to_lcd_representation(8))
+        output = out.getvalue()
+        assert output == " _ \n|_|\n|_|\n"
+
+
+def test_displays_the_number_nine_on_the_lcd():
+    with captured_output() as (out, err):
+        print_number_from_representation(number_to_lcd_representation(9))
+        output = out.getvalue()
+        assert output == " _\n|_|\n  |\n"
+
+
+def test_displays_the_number_zero_on_the_lcd():
+    with captured_output() as (out, err):
+        print_number_from_representation(number_to_lcd_representation(0))
+        output = out.getvalue()
+        assert output == " _ \n| |\n|_|\n"
+
+
+def test_displays_the_number_ten_on_the_lcd():
+    with captured_output() as (out, err):
+        print_number_from_representation(number_to_lcd_representation(10))
+        output = out.getvalue()
+        assert output == "   _ \n| | |\n| |_|\n"
 
 
 if __name__ == '__main__':
